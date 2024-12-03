a9523e1a36af18260d15b5bf0edc8723d805bca1
Abys31 <asmaa_yasmine.belarbi@etud.univ-jfc.fr>
2024-12-03 15:49:04 +0100

    first commit

---
+++ Placement.java
@@ -0,0 +1,13 @@
+public class Placement {
+    private float pos_x;
+    private float pos_y;
+    public Deplacement(float pos_x, float pos_y) {
+        this.pos_x = pos_x;
+        this.pos_y = pos_y;
+    }
+     public void Distance(){
+        int distance = 0;
+        
+
+     }
+}
