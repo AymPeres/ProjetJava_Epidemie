364d3deec9b1aaf0dd92cc566a1b7a3bea886d79
yasmin_belarbi <asmaa_yasmine.belarbi@etud.univ-jfc.fr>
2024-12-13 07:44:53 +0100

    Update Placement.java

--- Placement.java
+++ Placement.java
@@ -1,49 +1,55 @@
 public class Placement {
-  
+ 
     private float x;
-    private double y;
+    private float y;
+
 
     public Placement() {
-        this.x = 0.0;
-        this.y = 0.0;
+        this.x = 0;
+        this.y = 0;
     }
 
-    public Placement(double x, double y) {
+
+    public Placement(float x, float y) {
         this.x = x;
         this.y = y;
     }
 
-    public double getX() {
+
+    public float getX() {
         return x;
     }
 
-    public void setX(double x) {
+
+    public void setX(float x) {
         this.x = x;
     }
 
-    public double getY() {
+
+    public float getY() {
         return y;
     }
 
-    public void setY(double y) {
+
+    public void setY(float y) {
         this.y = y;
     }
 
-    public double calculerDistance(Placement autrePosition) {
-        Math.sqrt(Math.pow(this.x - autrePosition.x, 2) + Math.pow(this.y - autrePosition.y, 2));
+
+    public double distance(Placement p) {
+        double d = Math.sqrt(Math.pow(this.x - p.x, 2) + Math.pow(this.y - p.y, 2));
         return d ;
     }
 
+
     public String toString() {
-        return "Position{x=" + x + ", y=" + y + '}';
+        return "Position est x=" + x + ", y=" + y;
     }
 
 
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Placement position = (Placement) o;
-        return Double.compare(position.x, x) == 0 && 
-               Double.compare(position.y, y) == 0;
+
+
+    public boolean equals(Placement o) {
+        return Float.compare(this.x, o.x) == 0 &&  Float.compare(this.y, o.y) == 0;
     }
 }
