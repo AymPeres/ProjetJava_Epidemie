605008e7ae06cfc37959fe5b7d12b29b0fd338d3
yasmin_belarbi <asmaa_yasmine.belarbi@etud.univ-jfc.fr>
2024-12-13 07:43:53 +0100

    Update Personne.java

--- Personne.java
+++ Personne.java
@@ -1,39 +1,55 @@
 public class Personne {
     private String nom;
-    private String etat;
+    private EtatPersonne etat;
     private int facteurTransmission;
     private float dResistance;
     private Placement placement;
+    private TypeComportement comportement;
 
-    public Personne(String nom, String etat, int facteurTransmission, float dResistance) {
+
+
+
+    public Personne(String nom, EtatPersonne etat, int facteurTransmission, float dResistance) {
         this.nom = nom;
         this.etat = etat;
         this.facteurTransmission = facteurTransmission;
         this.dResistance = dResistance;
     }
 
+
     public getEtat(){
-        retuen etat;
+        return etat;
     }
+
+
 
 
     public void mourir() {
 
+
     }
+
 
     public double deplacer() {
 
+
     }
+
 
     public double distanceEuclidienneP2(Personne p) {
         return placement.distance(p.placement);
     }
 
+
     public double PContagion(Personne autre, double tauxTransmissionInitial, double distanceMax) {
         double distance = distanceEuclidienneP2(autre);
+        if(comportement==TypeComportement.AGRESSIF){
+            distance=distance*2;
+        }
         if (distance > distanceMax) {
             return 0;
         }
         return Math.max(0, tauxTransmissionInitial * (1 - (distance / distanceMax)));
     }
 }
+
