a00d9cd39491ed1abed9240627fd27ccd1883950
NeirouzAttia <neirouz.attia@etud.univ-jfc.fr>
2024-12-13 13:08:47 +0100

    Update Placement.java

--- Placement.java
+++ Placement.java
@@ -1,55 +1,45 @@
 public class Placement {
- 
+  
     private float x;
     private float y;
-
 
     public Placement() {
         this.x = 0;
         this.y = 0;
     }
 
-
     public Placement(float x, float y) {
         this.x = x;
         this.y = y;
     }
 
-
-    public float getX() {
+    public double getX() {
         return x;
     }
-
 
     public void setX(float x) {
         this.x = x;
     }
 
-
-    public float getY() {
+    public double getY() {
         return y;
     }
-
 
     public void setY(float y) {
         this.y = y;
     }
 
-
     public double distance(Placement p) {
-        double d = Math.sqrt(Math.pow(this.x - p.x, 2) + Math.pow(this.y - p.y, 2));
+        double d= Math.sqrt(Math.pow(this.x - p.x, 2) + Math.pow(this.y - p.y, 2));
         return d ;
     }
 
-
     public String toString() {
-        return "Position est x=" + x + ", y=" + y;
+        return "Placement est " + x + ", y=" + y;
     }
 
 
-
-
     public boolean equals(Placement o) {
-        return Float.compare(this.x, o.x) == 0 &&  Float.compare(this.y, o.y) == 0;
+        return Float.compare(o.x, x) == 0 && Float.compare(o.y, y) == 0;
     }
 }
